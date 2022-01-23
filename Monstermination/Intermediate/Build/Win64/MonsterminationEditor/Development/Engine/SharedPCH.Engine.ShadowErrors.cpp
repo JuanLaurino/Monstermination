249a1461f15a29cff317a261175599ac0d789e8b
JuanLaurino <juan.laurino@protonmail.com>
2022-01-23 19:48:05 +0100

    .gitignore arreglado Ivanna

--- Monstermination/Intermediate/Build/Win64/MonsterminationEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ Monstermination/Intermediate/Build/Win64/MonsterminationEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.29.30133
-#include "D:/Monstermination/Monstermination/Intermediate/Build/Win64/MonsterminationEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+// Compiler: 14.29.30037
+#include "D:/Proyectos/UE4/Monstermination/Monstermination/Intermediate/Build/Win64/MonsterminationEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
